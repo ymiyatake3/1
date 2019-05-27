3d383f36a84bc17666d64d6749389b90f09aa372
yoko <miyatakeyouko@gmail.com>
2019-05-27 17:08:20 +0900

    removed implements in Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,16 +1,4 @@
-import java.util.Arrays;
-import java.util.TreeMap;
 import java.util.Scanner;
-
-import java.lang.String;
-import java.lang.System;
-import java.lang.Math;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.FileNotFoundException;
 
 public class Anagram {
 
