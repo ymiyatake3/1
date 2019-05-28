1f3263f509c095a772d0ebdadac48b1d931ab2cc
yoko <miyatakeyouko@gmail.com>
2019-05-28 21:16:50 +0900

    added Scrap.java

--- Scrap.java
+++ Scrap.java
@@ -5,19 +5,22 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 class Scrap {
+
     public static void main(String[] args) {
 
         try {
             Document doc = Jsoup.connect("https://icanhazwordz.appspot.com").get();
+            
+            
+            // ここから下は適当
             Elements elm = doc.select("title");
-
-            for (Elements elms : elm) {
+            for(Element elms : elm) {
                 String title = elms.text();
                 System.out.println(title);
             }
-        } catch (IOException e) {
+
+        }catch(IOException e) {
             e.printStackTrace();
         }
     }
